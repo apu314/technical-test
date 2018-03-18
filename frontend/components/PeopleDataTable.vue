ff0bb5a878222b5112cbcb11a3dca011b7ba89a4
apu314 <apu314@apu314.com>
2018-03-18 15:34:17 +0000

    Fixed problem displaying the enabled colours in the editing dialog

--- frontend/components/PeopleDataTable.vue
+++ frontend/components/PeopleDataTable.vue
@@ -9,44 +9,34 @@
         <v-card-text>
           <v-container grid-list-md>
             <v-layout wrap>
-              <v-flex xs12 sm6 md4>
+              <v-flex xs12 sm6 md6>
                 <v-text-field label="First Name" v-model="editedItem.firstName" readonly></v-text-field>
               </v-flex>
-              <v-flex xs12 sm6 md4>
+              <v-flex xs12 sm6 md6>
                 <v-text-field label="Last Name" v-model="editedItem.lastName" readonly></v-text-field>
               </v-flex>
-              <v-flex xs12 sm6 md4>
-                <v-text-field label="Enabled" v-model="editedItem.enabled"></v-text-field>
+              <v-flex xs12 sm6 md6>
+                <v-switch
+                        label="Enabled"
+                        v-model="editedItem.enabled"
+                        :input-value="editedItem.enabled"
+                        hide-details
+                ></v-switch>
               </v-flex>
-              <v-flex xs12 sm6 md4>
-
+              <v-flex xs12 sm6 md6>
                 <v-switch
                         label="Authorised"
                         v-model="editedItem.authorised"
                         :input-value="editedItem.authorised"
                         hide-details
                 ></v-switch>
-
-                <v-text-field label="Authorised" v-model="editedItem.authorised"></v-text-field>
               </v-flex>
-              <v-flex xs12 sm6 md4>
+              <v-flex xs12 sm6 md4 v-for="(colour, index) in editedItem.colours">
                 <v-checkbox
                   row
-                  v-model="cbxColours.cbxColourRed"
-                  label="Red"
-                  input-value="1"
-                ></v-checkbox>
-                <v-checkbox
-                  row
-                  v-model="cbxColours.cbxColourGreen"
-                  label="Green"
-                  input-value="2"
-                ></v-checkbox>
-                <v-checkbox
-                  row
-                  v-model="cbxColours.cbxColourBlue"
-                  label="blue"
-                  input-value=""
+                  v-model="editedItem.colours[index].enabled"
+                  :label="colour.name"
+                  :input-value="colour.enabled"
                 ></v-checkbox>
               </v-flex>
             </v-layout>
@@ -54,7 +44,7 @@
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" flat @click.native="dialog = false">Cancel</v-btn>
+          <v-btn color="blue darken-1" flat @click.native.stop="closeDialog">Cancel</v-btn>
           <v-btn color="blue darken-1" flat @click.native="save">Save</v-btn>
         </v-card-actions>
       </v-card>
@@ -69,12 +59,12 @@
       <template slot="items" slot-scope="person">
         <tr>
           <td>{{ person.item.id }}</td>
-          <td>{{ person.item.firstName }} {{ person.item.lastName }}</td>
+          <td>{{ setfullName(person.item.firstName, person.item.lastName) }}</td>
           <td>{{ person.item.lastName }}</td>
           <td>
-            <span>{{ reverseName(person.item.firstName + person.item.lastName, person.item) }}</span>
-            <span v-if="!person.item.palindrome"><v-icon color="red">clear</v-icon></span>
+            <span v-if="!palindrome(person.item.firstName, person.item.lastName)"><v-icon color="red">clear</v-icon></span>
             <span v-else><v-icon color="green">done</v-icon></span>
+            <span :v-model="reverseName(person.item.firstName + person.item.lastName, person.item)">{{ reverseName(person.item.firstName + person.item.lastName, person.item) }}</span>
           </td>
           <td>{{ person.item.enabled }}</td>
           <td>{{ person.item.valid }}</td>
@@ -86,11 +76,8 @@
             </template>
           </td>
           <td class="justify-center layout px-0">
-            <v-btn icon class="mx-0" @click="editingItem(person.item)">
+            <v-btn icon class="mx-0" slot="activator" @click="editingItem(person.item)">
               <v-icon color="teal">edit</v-icon>
-            </v-btn>
-            <v-btn icon class="mx-0" @click="">
-              <v-icon color="pink">delete</v-icon>
             </v-btn>
           </td>
         </tr>
@@ -104,6 +91,37 @@
 
 <script>
 export default {
+  props: {
+    editedItem: {
+      default: function () {
+        return {
+          id: 0,
+          firstName: '',
+          lastName: '',
+          enabled: '',
+          valid: '',
+          authorised: '',
+          colours: [
+            {
+              id: 1,
+              name: 'Blue',
+              enabled: false
+            },
+            {
+              id: 2,
+              name: 'Red',
+              enabled: false
+            },
+            {
+              id: 3,
+              name: 'Green',
+              enabled: false
+            }
+          ]
+        }
+      }
+    }
+  },
   data () {
     return {
       dialog: false,
@@ -118,74 +136,106 @@ export default {
         { text: 'Colours', value: 'id', sortable: false },
         { text: 'Actions', value: 'id', sortable: false }
       ],
-      editedIndex: -1,
-      editedItem: [
-        {
-          id: 0,
-          firstName: '',
-          lastName: '',
-          enabled: '',
-          authorised: '',
-          colours: [
-            {
-              id: 1,
-              name: 'Red',
-              enabled: false
-            },
-            {
-              id: 2,
-              name: 'Green',
-              enabled: false
-            },
-            {
-              id: 3,
-              name: 'Blue',
-              enabled: false
-            }
-          ]
-        }
-      ],
-      cbxColours: [
-        {
-          cbxColourRed: false,
-          cbxColourGreen: false,
-          cbxColourBlue: false
-        }
-      ]
+      editedIndex: -1
     }
   },
   computed: {
     formTitle () {
       return this.editedIndex === -1 ? 'New Person' : 'Edit Person'
     },
+    /*
+    newPerson () {
+      return this.$options.propsData
+    },
+    */
     people () {
       var list = this.$store.state.people.list
-      for (var i in list) {
-        var fullName = list[i].firstName + list[i].lastName
-        var reverse = this.reverseName(fullName)
-        var fullNameL = fullName.toLowerCase()
-        var reverseL = reverse.toLowerCase()
-        if (fullNameL === reverseL) {
-          this.$set(list[i], 'palindrome', true)
-        } else {
-          this.$set(list[i], 'palindrome', false)
-        }
-      }
+
       return list
     }
   },
   methods: {
-    reverseName (fullName) {
-      var reverse = fullName.split('').reverse().join('')
+    closeDialog () {
+      this.editedIndex = -1
+      this.dialog = false
+      console.log(this.editedItem)
+    },
+    setfullName (firstName, lastName) {
+      this.fullName = firstName + lastName
+      return this.fullName
+    },
+    reverseName () {
+      var reverse = this.fullName.split('').reverse().join('')
 
       return reverse
     },
+    palindrome (firstName, lastName) {
+      var fullName = firstName + lastName
+      var reverse = this.reverseName(fullName)
+      var fullNameL = fullName.toLowerCase()
+      var reverseL = reverse.toLowerCase()
+      if (fullNameL === reverseL) {
+        return true
+      } else {
+        return false
+      }
+    },
     editingItem (person) {
       this.editedIndex = this.people.indexOf(person)
-      this.editedItem = person
-      this.dialog = true
+
+      this.editedItem.id = person.id
+      this.editedItem.firstName = person.firstName
+      this.editedItem.lastName = person.lastName
+      this.editedItem.enabled = person.enabled
+      this.editedItem.valid = person.valid
+      this.editedItem.authorised = person.authorised
       console.log(this.editedItem)
+      var editedColours = this.editedItem.colours
+      for (var colour in editedColours) {
+        for (var pColour in person.colours) {
+          if (editedColours[colour].name === person.colours[pColour].name) {
+            console.log('Colours match. \n editedColour: ' + editedColours[colour].name + '\n person: ' + person.colours[pColour].name)
+            this.editedItem.colours[colour].enabled = true
+            console.log(this.editedItem.colours[colour])
+            break
+          } else {
+            console.log('Colours doesn\'t match. \n editedColour: ' + editedColours[colour].name + '\n person: ' + person.colours[pColour].name)
+            this.editedItem.colours[colour].enabled = false
+            console.log(this.editedItem.colours[colour])
+            continue
+          }
+        }
+      }
+      // checking colours
+      // for (var editColour in this.editedItem.colours) {
+      // console.log(this.editedItem.colours)
+      // for (var personColour in person.colours) {
+      //   if (!editColour.id === person.colours[personColour].id) {
+      //     console.log(this.editedItem.colours[editColour.id])
+      //   } else {
+      //     this.editedItem.colours[editColour].id = person.colours[personColour].id
+      //     this.editedItem.colours[editColour.id].name = person.colours[personColour].name
+      //     this.editedItem.colours[editColour.id].enabled = person.colours[personColour].enabled
+      //   }
+      // }
+      // }
+      // this.editedItem = person
+      this.dialog = true
+      console.log(this.$options.propsData)
     }
+    /*
+    createModifyPerson (person) {
+      axios.post('/person', {
+        person: this.
+      })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
+    */
   }
 }
 </script>
